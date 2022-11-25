cb4e4d87e50e6dfa0ba6f6298a9f71a632764186
Mohd Faizan <gitfaizan0496@gmail.com>
2022-11-25 22:27:11 +0530

    fetch api and render a card for to show in a index.html page

--- index.js
+++ index.js
@@ -11,5 +11,31 @@ searchText.addEventListener("input", (e) => {
 
     const data = await response.json();
     console.log(data);
+    // convert json into json object
+    let results = JSON.parse(localStorage.getItem("results")) || [];
+    results.push(data);
+    //  pass the json object into string
+    localStorage.setItem("results", JSON.stringify(results));
+    //  extract data of that objects and use in below
+    const Title = data.Title;
+    const Poster = data.Poster;
+    currentMovieLists = data;
+    moviesContainer.innerHTML += `  <div mt-5 class="card-body " >
+     <h5  style="color:white; text-align:justify ;margin-top :10px" class="card-title">${Title}</h5>
+    
+     <img mt-3 class="animate" src="${Poster}">
+   
+     <button class="btn btn-dark" id="${
+       results.length - 1
+     }" name=${JSON.stringify(
+      data
+    )} onclick="favMovieItems(event)">Favourite</button>
+     <button class="btn btn-dark" id="${
+       results.length - 1
+     }" onclick=movieInfo(event)>Info</button>
+     
+   
+   </div>`;
   };
+  fetchApi();
 });
