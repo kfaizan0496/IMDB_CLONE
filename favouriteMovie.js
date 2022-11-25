4e0c3553033adaac34982b943de3eafa865cc4ad
Mohd Faizan <gitfaizan0496@gmail.com>
2022-11-25 22:51:53 +0530

    rendering elements to the favouriteMoviesList.html when user click favourites button

--- favouriteMovie.js
+++ favouriteMovie.js
@@ -8,4 +8,25 @@ function renderMovie() {
   console.log("favList", favMovieArray);
 
   let savedMovie = JSON.parse(favMovieArray);
+  if (savedMovie == null) {
+    favMovieArray = [];
+  } else {
+    savedMovie.forEach(function (el) {
+      const Title = el.Title;
+      const Poster = el.Poster;
+
+      // for showing list of favourite movie
+      favm.innerHTML += ` 
+   <ul>
+    <li>
+    <div mt-5 class="card-body animate">
+    <h5 class="card-title">${Title}</h5>
+    <img src="${Poster}">
+    <button class="btn btn-dark" id="delete"><i class="fa-solid fa-trash"></i></button>
+  </div>
+  </li>
+  <ul>`;
+    });
+  }
 }
+renderMovie();
